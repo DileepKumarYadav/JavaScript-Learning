36970f617daa715eb955d779f13138f20d039e4c
Dileep Kumar Yadav <dk725013@gmail.com>
2024-04-30 18:24:51 +0000

    Learned Variable

---
+++ 01_Basic/variable.js
@@ -0,0 +1,14 @@
+const accountID = 144553;
+let accountEmail = "dileepy@gmail.com"
+var accountPassword = "12345";
+accountCity = "Delhi";  // considered as var variable
+let accountState;       // only variable declaration considered as a undefined data type
+
+// accountId = 2 //Not allowed
+
+accountEmail = "dk@gmail.com";
+accountPassword = "293857";
+accountCity = "patna";
+
+//console.log(accountEmail); it takes only 2 parameters
+console.table([accountEmail,accountPassword,accountCity,accountState]);
